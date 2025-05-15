856861af023fe33eae97a3a04c1f282102c7e8dd
umang2110 <ugonawala66@gmail.com>
2025-05-15 22:51:20 +0530

    Add files via upload

---
+++ C Practical/operators/operators.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	//+,-,*,/,%
+	int a=20;
+	int b=15;
+	float c,d,e,f,g;
+	c=a+b;
+	printf("a+b=%.1f\n",c);
+	d=a-b;
+	printf("a-b=%.1f\n",d);
+	e=a*b;
+	printf("a*b=%.1f\n",e);
+	f=a/b;
+	printf("a/b=%.1f\n",f);
+	g=a%b;
+	printf("a%b=%.1f\n",g);
+}
